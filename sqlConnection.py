a404dd4b0a3ae18ca9a82ee67bcf463c01c695c2
Anonymous <anon@Anonymouss-Air.home>
2020-12-24 04:09:13 +0000

    sqlConnection deleted

--- sqlConnection.py
+++
@@ -1,9 +0,0 @@
-# INSERT EXAMPLE
-# sql = "INSERT INTO Users (name,email, created_at, api_key) VALUES('teste','teste@teste.pt','{}','0000-0000-0000-0000')".format(datetime.date.today())
-# try:
-# 	cursor.execute(sql)
-# 	db.commit()
-#
-# except:
-# 	db.rollback()
-# db.close()
